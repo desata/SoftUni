4ea3f13a6dd879e32b87d592ffc2f6a3f8cb1beb
desata <desata@gmail.com>
2022-04-26 09:26:36 +0300

    Initial

---
+++ Softuni_Csharp_Advanced/StacksAndQueues/StacksAndQueuesLab/7.HotPotato/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hot potato is a game in which children form a circle and start passing a hot potato.
+            //The counting starts with the first kid.
+            //Every nth toss the child left with the potato leaves the game.
+            //When a kid leaves the game, it passes the potato along.
+            //This continues until there is only one kid left.
+            //Create a program that simulates the game of Hot Potato.
+            //Print every kid that is removed from the circle.
+            //In the end, print the kid that is left last.
+
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> kids = new Queue<string>(input);
+            int tosses = 0;
+
+            while (kids.Count > 1)
+            {
+                tosses++;
+                string kid = kids.Dequeue();
+
+                if (tosses == n)
+                {
+                    tosses = 0;
+                    Console.WriteLine($"Removed " + kid);
+
+
+                }
+                else
+                {
+                    kids.Enqueue(kid);
+                }
+
+            }
+
+            Console.WriteLine($"Last is " + kids.Dequeue());
+
+        }
+    }
+}
---
+++ Softuni_Csharp_Advanced/StacksAndQueues/StacksAndQueuesLab/PartyProfit_Pyhon/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PartyProfit_Pyhon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int companions_count = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int coins = (days * 50);
+
+            for (int i = 1; i <= days; i++)
+            {            
+            if (i % 15 == 0)
+            {
+                companions_count += 5;
+            }
+            if (i % 10 == 0)
+            {
+                companions_count -= 2;
+            }
+            if (i % 5 == 0)
+            {
+                coins += (companions_count * 20);
+                    if (i % 3 == 0)
+                    {
+                        coins -= (companions_count * 2);
+                    }
+            }
+            if (i % 3 == 0)
+            {
+                coins -= (companions_count * 3);
+            }
+            if (i % 1 == 0)
+            {
+                coins -= (companions_count * 2);
+            }
+            }
+
+            Console.WriteLine($"{companions_count} companions received {coins/(companions_count)} coins each.");
+        }
+    }
+}
