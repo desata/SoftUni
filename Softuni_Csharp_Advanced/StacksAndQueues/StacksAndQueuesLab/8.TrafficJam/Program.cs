4ef7317fffebfd24791ba4d35263caee56fb2726
desata <desata@gmail.com>
2022-04-27 17:03:08 +0300

    initial

--- Softuni_Csharp_Advanced/StacksAndQueues/StacksAndQueuesLab/8.TrafficJam/Program.cs
+++ Softuni_Csharp_Advanced/StacksAndQueues/StacksAndQueuesLab/8.TrafficJam/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _8.TrafficJam
 {
@@ -7,6 +9,39 @@ namespace _8.TrafficJam
         static void Main(string[] args)
         {
             //
+            int n = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            Queue<string> cars = new Queue<string>();
+            int counter = 0;
+
+            while (command != "end")
+            {
+                if (command != "green")
+                {
+                    cars.Enqueue(command);
+                }
+                else
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (cars.Count >0)
+                        {
+                       
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+
         }
     }
 }
