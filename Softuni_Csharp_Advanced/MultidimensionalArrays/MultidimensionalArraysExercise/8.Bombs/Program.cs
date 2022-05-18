41845d458ad66dba646bf1756929b83626477598
desata <desata@gmail.com>
2022-05-18 09:19:34 +0300

    initial commit

--- Softuni_Csharp_Advanced/MultidimensionalArrays/MultidimensionalArraysExercise/8.Bombs/Program.cs
+++ Softuni_Csharp_Advanced/MultidimensionalArrays/MultidimensionalArraysExercise/8.Bombs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _8.Bombs
 {
@@ -7,6 +8,38 @@ namespace _8.Bombs
         static void Main(string[] args)
         {
             //
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            FillMatrix(matrix, matrixSize);
+            Console.WriteLine();
+            Print(matrix);
+
+
         }
+        static void FillMatrix(int[,] matrix, int matrixSise)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+        }
+        static void Print(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
     }
 }
