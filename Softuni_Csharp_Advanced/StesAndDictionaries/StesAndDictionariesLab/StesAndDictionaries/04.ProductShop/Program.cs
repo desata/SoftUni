d9d99ad6668ad57b767ba734391e563e3071e03f
desata <desata@gmail.com>
2022-05-20 19:09:44 +0300

    initial

---
+++ Softuni_Csharp_Advanced/StesAndDictionaries/StesAndDictionariesLab/StesAndDictionaries/04.ProductShop/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.ProductShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            string[] input = Console.ReadLine().Split(", ");
+            SortedDictionary<string, Dictionary<string, double>> foodShop = new SortedDictionary<string, Dictionary<string, double>>();
+
+            while (input[0] != "Revision")
+            {
+                string shops = input[0];
+                string product = input[1];
+                double price = double.Parse(input[2]);
+
+                if (!foodShop.ContainsKey(shops))
+                {
+                    foodShop[shops] = new Dictionary<string,double>();
+
+                }
+                foodShop[shops][product] = price;
+
+
+                input = Console.ReadLine().Split(", ");
+            }
+
+            foreach (var shops in foodShop)
+            {
+                var product = shops.Value;
+
+                Console.WriteLine($"{shops.Key}->");
+
+                foreach (var item in product)
+                {
+                    Console.WriteLine($"Product: {item.Key}, Price: {item.Value}");
+                }
+            }
+            //{shop}->
+            //Product: {product}, Price: {price:D1}
+
+        }
+    }
+}
