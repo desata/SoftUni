d73bb5e2b0df403ee2b86cee2d42169e6486974b
desata <desata@gmail.com>
2022-05-20 17:39:50 +0300

    Count Same Values In Array

---
+++ Softuni_Csharp_Advanced/StesAndDictionaries/StesAndDictionariesLab/StesAndDictionaries/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that counts in a given array of double values the number of occurrences of each value. 
+
+            double[] array = Console.ReadLine().Split().Select(double.Parse).ToArray(); ;
+            Dictionary<double,int> numbers = new Dictionary<double,int>();
+
+            foreach (var item in array)
+            {
+                if (!numbers.ContainsKey(item))
+                {
+                    numbers.Add(item, 0);
+                }
+                numbers[item]++;
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+
+        }
+    }
+}
