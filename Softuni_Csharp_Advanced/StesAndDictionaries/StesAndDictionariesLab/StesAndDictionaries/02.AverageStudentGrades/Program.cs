01ec0106ce64a0010bfa3c88977d67797303a65e
desata <desata@gmail.com>
2022-05-20 18:03:16 +0300

    Average Student Grades

---
+++ Softuni_Csharp_Advanced/StesAndDictionaries/StesAndDictionariesLab/StesAndDictionaries/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.AverageStudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program, which reads a name of a student and his/her grades and adds them to the student record, then prints the students' names with their grades and their average grade.
+
+            int studentNumber = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < studentNumber; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string studentName = input[0];
+                decimal studentGrade = decimal.Parse(input[1]);
+
+                if (!students.ContainsKey(studentName))
+                {
+                    students.Add(studentName, new List<decimal>());
+                }
+                students[studentName].Add(studentGrade);
+
+            }
+            foreach (var student in students)
+            {
+                var grades = student.Value;
+                var average = grades.Average();
+                Console.Write($"{student.Key} -> ");
+                foreach (var grade in grades)
+                {
+                    Console.Write($"{grade:F2} ");
+                }
+                Console.WriteLine($"(avg: {average:F2})");
+            }
+        }
+    }
+}
