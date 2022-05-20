675e3169bc5f53801fc7a048f15326b93f3ab5b8
desata <desata@gmail.com>
2022-05-20 18:33:50 +0300

    initial

---
+++ Softuni_Csharp_Advanced/StesAndDictionaries/StesAndDictionariesLab/StesAndDictionaries/03.Largest3Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read a list of integers and print the largest 3 of them. If there are less than 3, print all of them.
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] sorted = input.OrderByDescending(x => x).ToArray();
+            int loop = 3;
+            if (sorted.Length < 3)
+            {
+                loop = sorted.Length;
+            }
+
+            for (int i = 0; i < loop; i++)
+            {
+                Console.Write($"{sorted[i]} ");
+            }
+
+        }
+    }
+}
