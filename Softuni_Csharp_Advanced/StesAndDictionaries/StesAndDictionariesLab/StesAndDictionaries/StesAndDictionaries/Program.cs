a4fc1a3fe5adcdc78ec7a131029ed37dd06c0b5f
desata <desata@gmail.com>
2022-05-23 20:58:23 +0300

    Update Program.cs

--- Softuni_Csharp_Advanced/StesAndDictionaries/StesAndDictionariesLab/StesAndDictionaries/StesAndDictionaries/Program.cs
+++ Softuni_Csharp_Advanced/StesAndDictionaries/StesAndDictionariesLab/StesAndDictionaries/StesAndDictionaries/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StesAndDictionaries
 {
@@ -10,6 +11,13 @@ namespace StesAndDictionaries
             fruits["banana"] = 2.20;
             fruits["apple"] = 1.40;
             fruits["kiwi"] = 3.20;
+            Console.WriteLine(string.Join(", ", fruits.Keys));
+
+
+            var sortedFruits = new SortedDictionary<string, double>();
+            fruits["kiwi"] = 4.50;
+            fruits["orange"] = 2.50;
+            fruits["banana"] = 2.20;
             Console.WriteLine(string.Join(", ", fruits.Keys));
 
         }
