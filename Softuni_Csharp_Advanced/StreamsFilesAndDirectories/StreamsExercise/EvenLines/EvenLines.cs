e132eb5e0cd32cb25dffc7e6a8474d63bc73da5d
desata <desata@gmail.com>
2022-06-02 19:59:20 +0300

    initial

--- Softuni_Csharp_Advanced/StreamsFilesAndDirectories/StreamsExercise/EvenLines/EvenLines.cs
+++ Softuni_Csharp_Advanced/StreamsFilesAndDirectories/StreamsExercise/EvenLines/EvenLines.cs
@@ -2,6 +2,10 @@
 namespace EvenLines
 {
     using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
     public class EvenLines
     {
         static void Main()
@@ -13,7 +17,33 @@ namespace EvenLines
 
         public static string ProcessLines(string inputFilePath)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            StreamReader reader = new StreamReader(inputFilePath);
+            StringBuilder sb = new StringBuilder();
+            char[] symbols = {'-', ',', '.', '!', '?'};
+
+            int counter = 0;
+            while (true)
+            {
+                string result = reader.ReadLine();
+
+                if (result == null)
+                {
+                    break;
+                }
+                if (counter %2 == 0)
+                {
+                    foreach (var symbol in symbols)
+                    {
+                        result = result.Replace(symbol, '@');
+                    }
+                    result = String.Join(" ", result.Split().Reverse());
+
+                    sb.AppendLine(result);
+                    counter++;
+                }
+            }
+            return sb.ToString().TrimEnd();
         }
     }
 }
---
+++ Softuni_Csharp_Advanced/StreamsFilesAndDirectories/StreamsExercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,59 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            StreamWriter writer = new StreamWriter(outputFilePath);
+            char[] symbols = { '-', ',', '.', '!', '?', '\'' };
+
+            using (reader)
+            {
+                using (writer)
+                {
+
+                    var line = reader.ReadLine();
+                    int index = 1;
+
+                    while (line != null)
+                    {
+
+                        int counterpunct = 0;
+                        int countChar = 0;
+
+
+                        foreach (char symbol in line)
+                        {
+                            if (symbols.Contains(symbol))
+                            {
+                                counterpunct++;
+                            }
+                                                        
+                            if (Char.IsLetter(symbol))
+                            {
+                                countChar++;
+                            }
+
+                        }
+                        writer.WriteLine($"Line {index}: {line} ({countChar}) ({counterpunct})");
+                        index++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
