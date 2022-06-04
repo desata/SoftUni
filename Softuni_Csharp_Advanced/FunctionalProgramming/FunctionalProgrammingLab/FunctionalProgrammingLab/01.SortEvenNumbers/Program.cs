8e08f89f45c770f41f6dc23afe0801d0c60558c4
desata <desata@gmail.com>
2022-06-04 17:56:52 +0300

    initial

--- Softuni_Csharp_Advanced/FunctionalProgramming/FunctionalProgrammingLab/FunctionalProgrammingLab/01.SortEvenNumbers/Program.cs
+++ Softuni_Csharp_Advanced/FunctionalProgramming/FunctionalProgrammingLab/FunctionalProgrammingLab/01.SortEvenNumbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01.SortEvenNumbers
 {
@@ -6,7 +8,17 @@ namespace _01.SortEvenNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create a program that reads one line of integers separated by ", ". Then prints the even numbers of that sequence sorted in increasing order.
+            //Hint: It is up to you what type of data structures you will use to solve this problem. Use a functional programming filter and sort the collection of numbers.
+
+
+            List<int> numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            
+            numbers = numbers.Where(x => x % 2 == 0).OrderBy(x => x).ToList();
+
+
+            Console.WriteLine(String.Join(", ", numbers));
+
         }
     }
 }
