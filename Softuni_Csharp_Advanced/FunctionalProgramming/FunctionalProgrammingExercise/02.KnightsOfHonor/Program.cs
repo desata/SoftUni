a17b1fe76bc05e8884d97a785dcec04077865213
desata <desata@gmail.com>
2022-06-05 10:34:37 +0300

    initial

--- Softuni_Csharp_Advanced/FunctionalProgramming/FunctionalProgrammingExercise/02.KnightsOfHonor/Program.cs
+++ Softuni_Csharp_Advanced/FunctionalProgramming/FunctionalProgrammingExercise/02.KnightsOfHonor/Program.cs
@@ -11,14 +11,12 @@ namespace _02.KnightsOfHonor
         {
             //Create a program that reads a collection of names as strings from the console, appends "Sir" in front of every name, and prints it back on the console. Use Action<T>.
 
+            Action<List<string>> printNamesWithSir = name =>
+            Console.WriteLine("Sir " + string.Join(Environment.NewLine + "Sir ", name));
             List<string> names = Console.ReadLine().Split().ToList();
 
-            Action<string> printNamesWithSir = (name) => Console.WriteLine($"Sir {name}");
-
-            foreach (string name in names)
-            {
-                printNamesWithSir(name);
-            }
+            printNamesWithSir(names);
+            
         }
     }
 }
