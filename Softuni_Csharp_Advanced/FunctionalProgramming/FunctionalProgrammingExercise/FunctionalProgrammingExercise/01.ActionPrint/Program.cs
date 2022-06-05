2edc83590e7d0abcb13102294d3ccc335eb97170
desata <desata@gmail.com>
2022-06-05 09:36:43 +0300

    initial commit
    Create a program that reads a collection of strings from the console and then prints them onto the console. Each name should be printed on a new line. Use Action<T>.

---
+++ Softuni_Csharp_Advanced/FunctionalProgramming/FunctionalProgrammingExercise/FunctionalProgrammingExercise/01.ActionPrint/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that reads a collection of strings from the console and then prints them onto the console. Each name should be printed on a new line. Use Action<T>.
+
+            List<string> names = Console.ReadLine().Split(" ").ToList();
+            Action<string> printName = (name) => Console.WriteLine(name);
+            foreach (string name in names)
+            {
+                printName(name);
+            }
+        }
+    }
+}
+
