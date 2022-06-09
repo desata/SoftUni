4dc8f070dbf2905059b017e9146faf681a159ed3
desata <desata@gmail.com>
2022-06-09 10:53:13 +0300

    initial commit

---
+++ Softuni_Csharp_Advanced/06.DefiningClasses/DefiningClassesExercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    internal class DateModifier
+    {
+        public static int CalculateDifference(string firstDate, string secondDate)
+        {
+            DateTime first = DateTime.Parse(firstDate);
+            DateTime second = DateTime.Parse(secondDate);
+
+            int result = Math.Abs((first - second).Days);
+                       
+            return result;
+
+        }
+    }
+}
---
+++ Softuni_Csharp_Advanced/06.DefiningClasses/DefiningClassesExercise/05.DateModifier/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Create a class DateModifier, which stores the difference of the days between two dates. It should have a method that takes two string parameters representing a date as strings and calculates the difference in the days between them
+
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            int result = DateModifier.CalculateDifference(firstDate, secondDate);
+
+            Console.WriteLine(result);
+        }
+    }
+}
