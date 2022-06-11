05e32e0be76704b5c82a4ffb7945856bcc14b840
desata <desata@gmail.com>
2022-06-11 18:05:43 +0300

    initial

---
+++ Softuni_Csharp_Advanced/07.LinkedList/LinkedList/ImplementingLinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImplementingLinkedList
+{
+    public class Program
+    {
+
+        public static void Main()
+        { 
+        
+            var linkedList = new DoublyLinkedList<int>();
+
+            linkedList.AddHead(1);
+            linkedList.AddHead(2);
+            linkedList.AddHead(3);
+            linkedList.AddTail(4);
+
+            linkedList.ForEach(n => Console.WriteLine(n));
+
+        }
+
+    }
+}
