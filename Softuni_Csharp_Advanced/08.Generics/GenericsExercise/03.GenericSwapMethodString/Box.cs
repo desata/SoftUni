825d97729a0f25c2907691427a61fa889587d1e7
desata <desata@gmail.com>
2022-06-20 19:57:15 +0300

    initial

---
+++ Softuni_Csharp_Advanced/08.Generics/GenericsExercise/03.GenericSwapMethodString/Box.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.GenericSwapMethodString
+{
+    public class Box<T>
+    {
+        public Box(T element)
+        {
+            Element = element;
+        }
+
+        public Box(List<T> elementsList)
+        {
+        Elements = elementsList;
+        }
+
+        public T Element { get; }
+
+        public List<T> Elements { get; }
+
+        public void Swap(List<T> elements, int indexOne, int indexTwo)
+        {
+            T firstElement = elements[indexOne];
+            elements[indexOne] = elements[indexTwo];
+            elements[indexTwo] = firstElement;
+
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (T element in Elements)
+            {
+                sb.AppendLine($"{element.GetType()}: {element}");
+            }
+
+            return sb.ToString().TrimEnd();
+
+            // return base.ToString();
+            //return $"{typeof(T)}: {Element}";
+        }
+    }
+}
---
+++ Softuni_Csharp_Advanced/08.Generics/GenericsExercise/03.GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.GenericSwapMethodString
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            //Create a generic method that receives a list, containing any type of data and swaps the elements at two given indexes.
+
+            int n = int.Parse(Console.ReadLine());
+            var list = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(input);
+            }
+
+            var box = new Box<string>(list);
+            var indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            box.Swap(list, indexes[0], indexes[1]);
+
+            Console.WriteLine(box);
+        }
+    }
+}
