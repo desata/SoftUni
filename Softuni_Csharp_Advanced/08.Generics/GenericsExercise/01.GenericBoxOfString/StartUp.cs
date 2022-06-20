d5c87ee9845256dc2515cd5555194ea46f2f88e0
desata <desata@gmail.com>
2022-06-20 11:30:36 +0300

    initial

---
+++ Softuni_Csharp_Advanced/08.Generics/GenericsExercise/01.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            //Create a generic class Box that can be initialized with any type and store the value. Override the ToString() method and print the type and stored value.
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                var box = new Box<string>(input);
+                Console.WriteLine(box);
+
+            }
+
+        }
+    }
+}
