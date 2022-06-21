9ce836e5af569ab9ef678e8c350ef230b221d7e9
desata <desata@gmail.com>
2022-06-21 17:49:38 +0300

    initial

--- Softuni_Csharp_Advanced/08.Generics/GenericsExercise/07.Tuple/StartUp.cs
+++ Softuni_Csharp_Advanced/08.Generics/GenericsExercise/07.Tuple/StartUp.cs
@@ -8,22 +8,33 @@ namespace _07.Tuple
         {
             var personInfo = Console.ReadLine().Split();
             var fullName = $"{personInfo[0]} {personInfo[1]}";
-            var city = $"{personInfo[2]}";
+            var address = $"{personInfo[2]}";
+            var city = $"{personInfo[3]}";
 
             var nameAndBeer = Console.ReadLine().Split();
             var name = nameAndBeer[0];
             var beerLitters = int.Parse(nameAndBeer[1]);
+            var Isdrunk = false;
+            if (nameAndBeer[2] == "drunk")
+            {
+                Isdrunk = true;
+            }
+            else
+            {
+                Isdrunk = false;
+            };
 
             var numerInput = Console.ReadLine().Split();
-            var intNum = int.Parse(numerInput[0]);
+            var intNum = numerInput[0];
             var doubleNum = double.Parse(numerInput[1]);
+            var bank = numerInput[2];
 
 
-            Tuple<string, string> firstTuple = new Tuple<string, string>(fullName, city);
+            Treeuple<string, string, string> firstTuple = new Treeuple<string, string, string>(fullName, address, city);
 
-            Tuple<string, int> secondTuple = new Tuple<string, int>(name, beerLitters);
+            Treeuple<string, int, bool> secondTuple = new Treeuple<string, int, bool>(name, beerLitters, Isdrunk);
 
-            Tuple<int, double> thirddTuple = new Tuple<int, double>(intNum, doubleNum);
+            Treeuple<string, double, string> thirddTuple = new Treeuple<string, double, string>(intNum, doubleNum, bank);
 
             Console.WriteLine(firstTuple);
             Console.WriteLine(secondTuple);
---
+++ Softuni_Csharp_Advanced/09.IteratorsAndComparators/IteratorsAndComparators/01.ListyIterator/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string command = "";
+            ListyIterator<string> listy = null;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var argument = command.Split();
+
+                if (argument[0] == "Create")
+                {
+                    listy = new ListyIterator<string>(argument.Skip(1).ToArray());
+                    
+                }
+                else if (argument[0] == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+                else if (argument[0] == "Print")
+                {
+                    listy.Print();
+                }
+                else if (argument[0] == "HasNext")
+                {
+                    Console.WriteLine(listy.HasNext());
+                }
+            }
+        }
+    }
+}
