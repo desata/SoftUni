b97893a65056ce844de21c5813a363e88d874603
desata <desata@gmail.com>
2022-07-02 19:18:59 +0300

    initial

---
+++ SoftUni_Csharp_OOP/Inheritance/Inheritance/Person/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        
+
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public virtual int Age 
+        {
+            get { return age; } 
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception();
+                }
+                age = value;
+            } 
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format($"Name: {this.Name}, Age: {this.Age}"));
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
--- SoftUni_Csharp_OOP/Inheritance/Inheritance/Person/StartUp.cs
+++ SoftUni_Csharp_OOP/Inheritance/Inheritance/Person/StartUp.cs
@@ -1,10 +1,26 @@
-﻿namespace Person
+﻿using System;
+
+namespace Person
 {
     public class StartUp
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            
 
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            if (age <= 15)
+            {
+                Child person = new Child(name, age);
+                Console.WriteLine(person);
+            }
+            else 
+            {
+                Person person = new Person(name, age);
+                Console.WriteLine(person);
+            }
         }
     }
 }
