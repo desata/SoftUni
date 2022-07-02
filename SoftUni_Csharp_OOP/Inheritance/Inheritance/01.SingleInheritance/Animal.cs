4b13b18f7a626c74382915d0ace47bc7a7132f11
desata <desata@gmail.com>
2022-07-02 15:35:49 +0300

    initial

---
+++ SoftUni_Csharp_OOP/Inheritance/Inheritance/01.SingleInheritance/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    internal class Animal
+    {
+
+        //Animal with a single public method Eat() that prints: "eating…"
+
+        public void Eat()
+        {
+            Console.WriteLine("eating…");
+        }
+    }
+}
