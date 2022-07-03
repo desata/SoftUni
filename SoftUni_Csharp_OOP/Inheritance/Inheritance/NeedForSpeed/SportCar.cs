2dea8050aabd68d1f4f8933e72d5e21ebc55be8f
desata <desata@gmail.com>
2022-07-03 15:00:25 +0300

    initial

---
+++ SoftUni_Csharp_OOP/Inheritance/Inheritance/NeedForSpeed/SportCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class SportCar : Car
+    {
+        double DefaultFuelConsumption = 10;
+        public SportCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+        public override double FuelConsumption => this.DefaultFuelConsumption;
+    }
+}
