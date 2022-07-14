d5269daece663fb8f5ff15369c525a7068b689d0
desata <desata@gmail.com>
2022-07-14 16:04:31 +0300

    initial

---
+++ SoftUni_Csharp_Basic/BasicCscarp/basic/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string movieName = Console.ReadLine();
+            int movieLasts = int.Parse(Console.ReadLine());
+            int breakLasts = int.Parse(Console.ReadLine());
+
+            var breakRemains = (breakLasts * 0.625);
+
+            if (breakRemains >= movieLasts)
+            {
+                Console.WriteLine($"You have enough time to watch {movieName} and left with {Math.Ceiling(breakRemains - movieLasts)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {movieName}, you need {Math.Ceiling(movieLasts - breakRemains)} more minutes.");
+            }
+
+        }
+    }
+}
---
+++ SoftUni_Csharp_OOP/Polymorphism/02.Animals/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, string favoriteFood)
+        {
+            Name = name;
+            FavoriteFood = favoriteFood;
+        }
+
+        public string Name { get; set; }
+        public string FavoriteFood { get; set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavoriteFood}";
+        }
+    }
+}
