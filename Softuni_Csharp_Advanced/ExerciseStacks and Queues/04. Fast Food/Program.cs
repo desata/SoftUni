b9fb84fd683101a23c1463cb1f7600d09c01958f
desata <desata@gmail.com>
2022-07-27 16:13:53 +0300

    initial

---
+++ Softuni_Csharp_Advanced/ExerciseStacks and Queues/04. Fast Food/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQty = int.Parse(Console.ReadLine());
+            List<int> foodList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Queue<int> food = new Queue<int>(foodList);
+
+            Console.WriteLine(food.Max());
+
+            while (food.Count > 0 && foodQty > 0)
+            {
+                if (foodQty >= food.Peek())
+                {
+                    foodQty -= food.Dequeue();
+                }
+                else
+                {
+                    Console.Write("Orders left: ");
+                    Console.Write(String.Join(" ", food));
+                    break;
+                }
+            }
+
+            if (food.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
---
+++ Softuni_Csharp_Advanced/ExerciseStacks and Queues/05. Fashion Boutique/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesInTheBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(clothesInTheBox);
+            
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int tempRackCapacity = rackCapacity;
+            int numberOfRacksNeeded = 1;
+
+            while (stack.Count > 0)
+            {
+                if (rackCapacity - stack.Peek() >= 0)
+                {
+                    rackCapacity -= stack.Pop();
+                }
+                else
+                {
+                    numberOfRacksNeeded++;
+                    rackCapacity = tempRackCapacity;
+                }
+            }
+
+            Console.WriteLine(numberOfRacksNeeded);
+
+        }
+    }
+}
