bba2cd5d68cc168829492b448a7798f8c7bfb840
desata <desata@gmail.com>
2022-07-27 11:16:48 +0300

    initial

---
+++ Softuni_Csharp_Advanced/ExerciseStacks and Queues/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nsx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>();
+            int n = nsx[0];
+            int s = nsx[1];
+            int x = nsx[2];
+
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(input[i]);
+            }
+            for (int j = 0; j < s; j++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
