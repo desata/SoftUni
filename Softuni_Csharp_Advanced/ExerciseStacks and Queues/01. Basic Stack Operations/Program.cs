bc90254ca238d4bb9a417b8138b642d996f4fa68
desata <desata@gmail.com>
2022-07-27 09:56:09 +0300

    initial

---
+++ Softuni_Csharp_Advanced/ExerciseStacks and Queues/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nsx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nsx[0];
+            int s = nsx[1];
+            int x = nsx[2];
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(input[i]);
+            }
+            for (int j = 0; j < s; j++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
