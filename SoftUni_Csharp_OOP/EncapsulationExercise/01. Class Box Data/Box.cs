e37db139b7c198e0423544bbade874df8b038209
desata <desata@gmail.com>
2022-09-28 10:19:24 +0300

    initial

---
+++ SoftUni_Csharp_OOP/EncapsulationExercise/01. Class Box Data/Box.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBox
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Length cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Width cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Height cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+
+        public double SurfaceArea()
+        {
+            return 2 * length * width + 2 * length * height + 2 * width * height;
+
+        }
+        //Calculate and return the surface area of the Box.
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * length * height + 2 * width * height;
+        }
+        //Calculate and return the lateral surface area of the Box.
+
+        public double Volume()
+        {
+            return length * width * height;
+        }
+        //Calculate and return the volume of the Box.
+
+
+
+    }
+}
---
+++ SoftUni_Csharp_OOP/EncapsulationExercise/01. Class Box Data/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassBox
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double heigth = double.Parse(Console.ReadLine());
+
+            Box box;
+            try
+			{
+				box = new Box(length, width, heigth);
+
+			}
+			catch (Exception ex)
+			{
+
+				Console.WriteLine(ex.Message);
+				return;
+			}
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+            Console.WriteLine($"Volume - {box.Volume():F2}");
+        }
+    }
+}
