e7b173aceb2dbf807e6a836b814a4bdc29a8c927
Desislava Nikolova <desata@gmail.com>
2023-01-13 09:35:24 +0200

    Update Built-in Functions.sql

--- Softuni_MSSQL/4.Built-in Functions/Built-in Functions.sql
+++ Softuni_MSSQL/4.Built-in Functions/Built-in Functions.sql
@@ -108,4 +108,43 @@ from Users
 where IPAddress like ('___.1%.%.___') 
 Order By Username
 
---
+--17. Show All Games with Duration & Part of the Day
+/*
+Find all games with part of the day and duration. 
+Sort them by game name alphabetically, then by duration (alphabetically, not by the timespan) and part of the day (all ascending). 
+Part of the Day should be Morning (time is >= 0 and < 12), Afternoon (time is >= 12 and < 18), Evening (time is >= 18 and < 24). 
+Duration should be Extra Short (smaller or equal to 3), Short (between 4 and 6 including), Long (greater than 6) and Extra Long (without duration). 
+*/
+select [Name],
+CASE
+	WHEN datepart(hh, Start) between 18 and 24 THEN 'Evening'
+	WHEN datepart(hh, Start) between 12 and 17 THEN 'Afternoon'
+	WHEN datepart(hh, Start) between 0 and 11 THEN 'Morning'
+END as [Part of the Day],
+CASE
+	WHEN Duration <=3 THEN 'Extra Short'
+	WHEN Duration between 4 and 6 THEN 'Short'
+	WHEN Duration >6 THEN 'Long'
+	ELSE 'Extra Long'
+END as Duration
+ 
+from Games
+order by [Name], Duration,[Part of the Day]
+
+--18. Orders Table
+/*
+You are given a table Orders(Id, ProductName, OrderDate) filled with data. 
+Consider that the payment for that order must be accomplished within 3 days after the order date. 
+Also the delivery date is up to 1 month. 
+Write a query to show each product's name, order date, pay and deliver due dates. 
+*/
+--[ProductName] [OrderDate]	[Pay Due]	[Deliver Due]
+select
+[ProductName]
+,[OrderDate] as [OrderDate]
+,DATEADD(day, 3, [OrderDate]) as [Pay Due]
+--или ,[OrderDate]+3 as [Pay Due]
+,DATEADD(month, 1, [OrderDate]) as [Deliver Due]
+from Orders
+
+
