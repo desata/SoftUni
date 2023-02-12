dd143979c3c2e8099c8556fcad04992a6d3bfca1
Desislava Nikolova <desata@gmail.com>
2023-02-12 12:06:01 +0200

    Update Triggers and Transactions.sql

--- Softuni_MSSQL/8.TriggersAndTransactions/Triggers and Transactions.sql
+++ Softuni_MSSQL/8.TriggersAndTransactions/Triggers and Transactions.sql
@@ -136,3 +136,121 @@ GO
 
 SELECT * FROM Accounts WHERE Id IN (1,2)
 EXEC usp_TransferMoney 1, 2, 1000
+
+/* 6.*Massive Shopping
+User Stamat in Safflower game wants to buy some items. He likes all items from Level 11 to 12
+as well as all items from Level 19 to 21. As it is a bulk operation you have to use transactions. 
+A transaction is the operation of taking out the cash from the user in the current game as well
+as adding up the items. 
+Write transactions for each level range. If anything goes wrong turn back the changes inside of the transaction.
+Extract all of Stamat's item names in the given game sorted by name alphabetically.
+*/
+
+--GameID = 87
+--StamatID = 9
+
+DECLARE @userGameId INT = (SELECT Id FROM UsersGames WHERE UserId = 9 AND GameId = 87)
+DECLARE @cash MONEY = (SELECT Cash FROM UsersGames WHERE Id = @userGameId)
+DECLARE @itemsPrice MONEY = (SELECT SUM(Price) AS TotalPrice FROM Items WHERE MinLevel BETWEEN 11 AND 12)
+
+IF @cash >= @itemsPrice
+BEGIN
+	BEGIN TRANSACTION
+	UPDATE UsersGames
+		SET Cash -= @itemsPrice
+		WHERE UserId = 9 AND GameId = 87
+
+	INSERT INTO UserGameItems (ItemId, UserGameId)
+	SELECT Id, @userGameId FROM Items WHERE MinLevel BETWEEN 11 AND 12
+	COMMIT
+END
+
+
+SET @cash = (SELECT Cash FROM UsersGames WHERE Id = @userGameId);
+SET @itemsPrice = (SELECT SUM(Price) AS TotalPrice FROM Items WHERE MinLevel BETWEEN 19 AND 21)
+
+IF @cash >= @itemsPrice
+BEGIN
+	BEGIN TRANSACTION
+	UPDATE UsersGames
+		SET Cash -= @itemsPrice
+		WHERE UserId = 9 AND GameId = 87
+
+	INSERT INTO UserGameItems (ItemId, UserGameId)
+	SELECT Id, @userGameId FROM Items WHERE MinLevel BETWEEN 19 AND 21
+	COMMIT
+END
+
+SELECT i.Name
+	FROM Users AS u
+	JOIN UsersGames AS ug
+	  ON ug.UserId = u.Id
+	JOIN Games AS g
+	  ON g.Id = ug.GameId	   
+	JOIN UserGameItems AS ugi
+	  ON ugi.UserGameId = ug.Id
+	JOIN Items AS i
+	  ON i.Id = ugi.ItemId
+	WHERE u.Username = 'Stamat' AND g.Name = 'Safflower'
+	ORDER BY i.Name
+
+
+
+/*
+8.	Employees with Three Projects
+Create a procedure usp_AssignProject(@emloyeeId, @projectID) that assigns projects to an employee.
+If the employee has more than 3 project throw exception and rollback the changes.
+The exception message must be: "The employee has too many projects!" with Severity = 16, State = 1.
+*/
+
+GO
+CREATE OR ALTER PROC usp_AssignProject(@emloyeeId INT, @projectID INT)
+AS
+BEGIN TRANSACTION
+	DECLARE @employee INT = (SELECT EmployeeID FROM Employees WHERE EmployeeID = @emloyeeId)
+	DECLARE @project INT = (SELECT ProjectID FROM Projects WHERE ProjectID = @projectID)
+	
+	DECLARE @countProjects INT = (SELECT COUNT(*)FROM EmployeesProjects 
+								  WHERE EmployeeID = @emloyeeId)
+
+	IF (@employee IS NULL OR @project IS NULL)
+	BEGIN
+		ROLLBACK;
+		THROW 50001, 'Invalid employee or project', 2
+	END
+	IF (@countProjects) >= 3
+	BEGIN
+		ROLLBACK;
+		THROW 50002, 'The employee has too many projects!', 1
+	END
+	INSERT INTO EmployeesProjects(EmployeeID, ProjectID) VALUES
+	(@emloyeeId, @projectID)
+COMMIT
+GO
+
+/*
+9.	Delete Employees
+Create a table Deleted_Employees(EmployeeId PK, FirstName, LastName, MiddleName, JobTitle, DepartmentId, Salary)
+that will hold information about fired (deleted) employees from the Employees table.
+Add a trigger to Employees table that inserts the corresponding information
+about the deleted records in Deleted_Employees.
+*/
+
+CREATE TABLE Deleted_Employees
+(
+	EmployeeId INT PRIMARY KEY IDENTITY,
+	FirstName VARCHAR(50) NOT NULL,
+	LastName VARCHAR(50) NOT NULL,
+	MiddleName VARCHAR(50),
+	JobTitle VARCHAR(150) NOT NULL,
+	DepartmentId INT REFERENCES Departments(DepartmentID),
+	Salary MONEY NOT NULL
+)
+
+GO
+CREATE TRIGGER tr_LogDeletedEmployees
+ON Employees FOR DELETE
+AS
+	INSERT INTO Deleted_Employees(FirstName, LastName, MiddleName, JobTitle, DepartmentId, Salary)
+	SELECT FirstName, LastName, MiddleName, JobTitle, DepartmentId, Salary FROM deleted
+GO
