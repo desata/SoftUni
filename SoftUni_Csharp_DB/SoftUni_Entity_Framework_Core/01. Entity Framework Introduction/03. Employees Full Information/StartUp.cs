ae1435a5a6fc7c0b1d84a3dd097cb78c8fa82df5
Desislava Nikolova <desata@gmail.com>
2023-03-01 17:17:19 +0200

    initial

---
+++ SoftUni_Csharp_DB/SoftUni_Entity_Framework_Core/01. Entity Framework Introduction/03. Employees Full Information/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System.Text;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           SoftUniContext dbContext = new SoftUniContext();
+            string result = GetEmployeesFullInformation(dbContext);
+            Console.WriteLine(result);
+
+            
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees.OrderBy(e => e.EmployeeId).ToArray();
+
+            foreach (var employee in employees) 
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+                
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
