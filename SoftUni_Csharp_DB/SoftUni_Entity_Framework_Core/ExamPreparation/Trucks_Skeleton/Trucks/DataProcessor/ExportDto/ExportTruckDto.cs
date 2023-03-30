fc1670298c072abe17ea4c7b51d0505fe5b22671
Desislava Nikolova <desata@gmail.com>
2023-03-30 13:51:09 +0300

    json and xml export

---
+++ SoftUni_Csharp_DB/SoftUni_Entity_Framework_Core/ExamPreparation/Trucks_Skeleton/Trucks/DataProcessor/ExportDto/ExportTruckDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+using System.Xml;
+
+namespace Trucks.DataProcessor.ExportDto
+{
+    [XmlType("Truck")]
+    public class ExportTruckDto
+    {
+        [XmlElement("RegistrationNumber")]
+        public string RegistrationNumber { get; set; } = null!;
+
+        [XmlElement("Make")]
+        public string Make { get; set; } = null!;
+    }
+}
