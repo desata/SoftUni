cce9f81f3eb2b50a2098d2ec5a9b85557674120c
Desislava Nikolova <desata@gmail.com>
2023-06-05 17:25:50 +0300

    ASP.NET Core Introduction

---
+++ SoftUni_Csharp_ASP.NET Fundamentals/ASP.NET Core Introduction/ASP.NET Core Introduction/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using ASP.NET_Core_Introduction.Models.Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Introduction.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IEnumerable<ProductViewModel> _products = new List<ProductViewModel>()
+        { 
+            new ProductViewModel()
+            { 
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 5.50
+            },
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 1.30
+            },
+        };
+    }
+
+    public IActionResult All()
+    {
+
+        return View();
+    }
+}
---
+++ SoftUni_Csharp_ASP.NET Fundamentals/ASP.NET Core Introduction/ASP.NET Core Introduction/Models/Product/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NET_Core_Introduction.Models.Product
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
