7ad3935b9c4b3404f7f9dbc74442eabb57cf8280
Desislava Nikolova <desata@gmail.com>
2024-01-16 17:05:06 +0200

    initial

---
+++ SoftUni_Exams_And_Preparation/LetterCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace LetterCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string value = "last_interest_pmt_amt";
+            foreach (char c in value)
+            {
+                Console.WriteLine((int)c);
+            }
+        }
+    }
+}
