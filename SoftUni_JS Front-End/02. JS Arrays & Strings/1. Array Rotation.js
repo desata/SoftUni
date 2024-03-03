69e9b17d9f59ea15db749828c880c50b5bfe98b6
Desislava Nikolova <desata@gmail.com>
2024-03-03 17:44:39 +0200

    Update 1. Array Rotation.js

--- SoftUni_JS Front-End/02. JS Arrays & Strings/1. Array Rotation.js
+++ SoftUni_JS Front-End/02. JS Arrays & Strings/1. Array Rotation.js
@@ -1,20 +1,21 @@
-function solve(arr, x) {
-    let n = arr.length;
- 
-    // if x is greater than length 
-    // of the array
-    x = x % n;
- 
-    let first_x_elements = arr.slice(0, x);
- 
-    let remaining_elements = arr.slice(x, n);
- 
-    // Destructuring to create the desired array
-    arr = [...remaining_elements, ...first_x_elements];
- 
-    console.log(arr);
+function solve(array, rotateNums) {
+    let result = '';
+
+    if(rotateNums >= array.length){
+        rotateNums = rotateNums % array.length
+    }
+    for (let i = rotateNums; i< array.length; i++){
+        result += array[i] + ' ';
+        } 
+
+        for (let i = 0; i< rotateNums; i++){
+            result += array[i] + ' ';
+        }
+        console.log(result);
 }
 
-solve([51, 47, 32, 61, 21], 2);
+solve([51, 47, 32, 61, 21], 62);
 solve([32, 21, 61, 1], 4);
 solve([2, 4, 15, 31], 5);
+
+
