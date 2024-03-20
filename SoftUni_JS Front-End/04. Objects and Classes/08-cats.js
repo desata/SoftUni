ad75a24959add6b47bbc4a392775f7ddfa7eed07
Desislava Nikolova <desata@gmail.com>
2024-03-20 14:35:39 +0200

    Create 08-cats.js

---
+++ SoftUni_JS Front-End/04. Objects and Classes/08-cats.js
@@ -0,0 +1,18 @@
+class Cat {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    meow() {
+        console.log(`${this.name}, age ${this.age} says Meow`);
+    }
+}
+
+function solve(input){
+    const result = input.map(line => line.split(' '))
+    .map(([name, age]) => new Cat(name, age))
+    .forEach(cat => cat.meow());
+}
+
+solve(['Mellow 2', 'Tom 5']);
+solve(['Candy 1', 'Poppy 3', 'Nyx 2']);
