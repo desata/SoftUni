8ec2cf5cf8dc20246ad96757e3ca630b7c0af0df
Desislava Nikolova <desata@gmail.com>
2024-03-22 10:09:48 +0200

    Create 01-employees.js

---
+++ SoftUni_JS Front-End/04. Objects and Classes/01-employees.js
@@ -0,0 +1,17 @@
+function solve(input){
+    const employees = [];
+
+    for (const record of input) {
+        employees.push(record);
+    }
+
+    console.log(employees);
+}
+
+solve([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+    ]
+    );
