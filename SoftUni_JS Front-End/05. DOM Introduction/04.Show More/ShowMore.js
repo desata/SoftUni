6d5ac8dc35f51de31c55253314c61c1556633593
Desislava Nikolova <desata@gmail.com>
2024-04-03 11:26:41 +0300

    Show more

---
+++ SoftUni_JS Front-End/05. DOM Introduction/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+   const readMore = document.getElementById('more');
+   const moreText = document.getElementById('text');
+
+   readMore.style.display = 'none';
+   moreText.style.display = 'inline';
+}
